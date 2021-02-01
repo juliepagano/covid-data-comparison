4e9a2d3ad19180a4d4573c5ee1fec907bbd6c977
Julie Pagano <julie.pagano@gmail.com>
2021-01-31 18:50:47 -0800

    Add more examples.

--- src/constants/naturalDisasters.ts
+++ src/constants/naturalDisasters.ts
@@ -1,7 +1,13 @@
 const naturalDisasters = [
   {
-    label: "Hurricane Katrina",
-    value: 1833,
+    label: "Hurricane Katrina (estimated)",
+    value: 1836,
+    source: "https://en.wikipedia.org/wiki/Hurricane_Katrina#Impact",
+  },
+  {
+    label: "1995 Chicago heat wave ",
+    value: 739,
+    source: "https://en.wikipedia.org/wiki/1995_Chicago_heat_wave",
   },
 ];
 export default naturalDisasters;
